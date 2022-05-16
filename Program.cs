7227231e45b0aabb68f8d742cde3f0654f334f98
Vanina Vilte / <88165310+vanina-vilte@users.noreply.github.com>
2022-05-16 16:32:48 -0300

    primer-commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ContadorDeVocales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            contadorDeVocales();
+
+            static void contadorDeVocales()
+            {
+                //realizar funciones para que sea más facil de leer
+                Console.WriteLine("Introduzca una palabra");
+                string palabra = Console.ReadLine().ToLower();
+
+                int contador = 0;
+                for (int i = 0; i < palabra.Length; i++)
+                {
+                    if (palabra[i] == 'a' || palabra[i] == 'e' || palabra[i] == 'i' || palabra[i] == 'o' || palabra[i] == 'u')
+                    {
+                        contador++;
+                    }
+
+                }
+
+                Console.WriteLine("Usted a utilizado " + contador + " vocales");
+            }
+        }
+    }
+}
